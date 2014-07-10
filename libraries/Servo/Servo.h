138388ea5d6665937548360339ee7115dd9771d4
mwidick <mwidick@mac.com>
2014-07-10 00:08:25 -0400

    Update Servo.h

--- libraries/Servo/Servo.h
+++ libraries/Servo/Servo.h
@@ -61,9 +61,9 @@
 
 #define _useTimer1 
 #define _useTimer3
-#define _useTimer4 
-typedef enum { _timer1, _timer3, _timer4, _Nbr_16timers } timer16_Sequence_t ;
-
+#define _useTimer4
+#define _useTimer5
+typedef enum { _timer1, _timer3, _timer4, _timer5, _Nbr_16timers } timer16_Sequence_t ;
 #elif defined(__AVR_ATmega32U4__)  
 #define _useTimer1 
 typedef enum { _timer1, _Nbr_16timers } timer16_Sequence_t ;
